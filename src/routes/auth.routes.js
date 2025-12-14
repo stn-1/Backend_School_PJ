9e6a4f3cb6521c8ca1350c83a25c98cc35ab6e33
stn-1 <nonamestn9@gmail.com>
2025-12-14 16:07:36 +0700

    user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,18 @@
 // routes/auth.routes.js
 import express from "express";
-import { 
-  register, 
-  login, 
-  requestRefreshToken, 
-  getProfile, 
+import {
+  register,
+  login,
+  requestRefreshToken,
+  getProfile,
   logout,
   updateProfile,
-  getProfilebyID
+  getProfilebyID,
+  searchUsers,
 } from "../controllers/auth.controller.js"; // Nhớ thêm .js nếu dùng ES Modules
 import uploadAvatar from "../middlewares/uploadAvata.middleware.js";
-import {updateAvatar} from "../controllers/auth.controller.js";
-import { verifyToken} from "../middlewares/auth.middleware.js";
-
+import { updateAvatar } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -28,7 +28,6 @@ router.post("/login", login);
 // (Lưu ý: Route này KHÔNG dùng verifyToken, vì Access Token đã hết hạn rồi mới gọi vào đây)
 router.post("/refresh", requestRefreshToken);
 
-
 // --- PROTECTED ROUTES (Phải có Access Token hợp lệ) ---
 
 // Lấy thông tin cá nhân: GET /api/auth/profile
@@ -38,7 +37,13 @@ router.get("/profile", verifyToken, getProfile);
 router.post("/logout", verifyToken, logout);
 
 // phần thay đổi avata
-router.post("/avatar", verifyToken , uploadAvatar.single("avatar"), updateAvatar);
-router.patch("/profile",verifyToken,updateProfile);
-router.get("/profile/:id",verifyToken,getProfilebyID)
+router.post(
+  "/avatar",
+  verifyToken,
+  uploadAvatar.single("avatar"),
+  updateAvatar
+);
+router.patch("/profile", verifyToken, updateProfile);
+router.get("/profile/:id", verifyToken, getProfilebyID);
+router.get("/search", verifyToken, searchUsers);
 export default router;
